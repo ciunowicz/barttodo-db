b1f224f572c7b92de76fa7cfba1efc6b556398c9
bartek <ciunowicz@hotmail.com>
2020-11-18 19:08:45 +0100

    add icons in navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,10 @@ import TypoGraphy from '@material-ui/core/Typography'
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import EditIcon from "@material-ui/icons/Edit";
+
 // import history from './history';
 
 const useStyles = makeStyles((theme) => ({
@@ -15,9 +19,17 @@ const useStyles = makeStyles((theme) => ({
     button: {
       marginRight: theme.spacing(2),
     }, 
-    rightBar: {
-      marginLeft: 'auto',
-  }, 
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    rightToolbar: {
+      marginLeft: "auto",
+      marginRight: -12
+    },
+  title: {
+    cursor: 'pointer',
+}, 
+
 }));
 
 function NavBar(props) {
@@ -33,12 +45,16 @@ function NavBar(props) {
       <div className={classes.root}>
       <AppBar color="primary" position="static" >
       <Toolbar>
-        <TypoGraphy  color="inherit" variant="h6">
-        Bart todo
-        </TypoGraphy>              
+      <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          
+          </IconButton>
+          <TypoGraphy variant="subtitle1" className={classes.title} color="inherit" onClick={()=>handleClick("/")}>
+          Barttodo
+        </TypoGraphy>          
            
-         <div className={classes.rightBar}>
-                <Button
+        <section className={classes.rightToolbar}>
+               {/*  <Button
                     variant="contained"
                     color="primary"
                     className={classes.button}
@@ -46,9 +62,13 @@ function NavBar(props) {
                     onClick={()=>handleClick("/add/")}
                 >
         Add
-      </Button>
+      </Button> */}
+
+<IconButton color="inherit" aria-label="Edit" onClick={()=>handleClick("/add/")}>
+            <EditIcon />
+          </IconButton>
                 
-                   <Button
+                   {/* <Button
                     variant="contained"
                     color="primary"
                     className={classes.button}
@@ -56,8 +76,12 @@ function NavBar(props) {
                     onClick={()=>handleClick("/")}
                 >
         List
-      </Button>
-      </div>
+      </Button> */}
+<IconButton color="inherit" aria-label="ListAlt"  onClick={()=>handleClick("/")}>
+            <ListAltIcon />
+          </IconButton>
+
+      </section>
       </Toolbar>
     </AppBar>         
     </div> 
